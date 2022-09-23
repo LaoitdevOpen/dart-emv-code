de2d6a8aab199aac99396aa3969fc97d9df84fb9
xang Ultimate <xang.ultimate@gmail.com>
2022-09-23 08:25:33 +0700

    🌈 style(test file): rename test

--- test/get_value_length_test.dart
+++ test/get_value_length_test.dart
@@ -4,33 +4,21 @@ import 'package:test/test.dart';
 void main() {
   test("get value length <= 10", () {
     final valLength = getValueLength("12345678");
-
-    print("value length ----> $valLength");
-
     expect(valLength, "08");
   });
 
   test("get value length >= 10", () {
-    final valLength = getValueLength("1234567890");
-
-    print("value length ----> $valLength");
-
+    final valLength = getValueLength("ABCDEFGHIJ");
     expect(valLength, "10");
   });
 
   test("format crc value length <= 10", () {
     final valLength = format("123", "12345678");
-
-    print("value ----> $valLength");
-
     expect(valLength, "1230812345678");
   });
 
   test("format crc value length >= 10", () {
     final crcValue = format("123", "1234567890");
-
-    print("value ----> $crcValue");
-
     expect(crcValue, "123101234567890");
   });
 }
